20220f0a5600e81e25d9615566f7cda54e27ceaa
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-04-15 21:00:28 +0000

    Introduce a new priorityConfigManager
    This config manager is disabled by default, and enabled by the temporary
    `logs_config.cca_use_bare_configs` config flag.  Without that flag, the
    existing simpleConfigManager is used.
    
    Currently, the priorityConfigManager does exactly the same thing as the
    simpleConfigManager, and the tests demonstrate that.  But it does so in
    a way that focuses on generation of configs on a per-service basis.
    This commit should be reviewed as a re-implementation of the existing
    algorithm, setting the stage for a simple change in the next commit.

--- pkg/autodiscovery/configmgr_test.go
+++ pkg/autodiscovery/configmgr_test.go
@@ -353,3 +353,7 @@ func (suite *ConfigManagerSuite) TestFuzz() {
 func TestSimpleConfigManagement(t *testing.T) {
 	suite.Run(t, &ConfigManagerSuite{factory: newSimpleConfigManager})
 }
+
+func TestPriorityConfigManagement(t *testing.T) {
+	suite.Run(t, &ConfigManagerSuite{factory: newPriorityConfigManager})
+}
