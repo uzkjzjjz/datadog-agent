1110f27c5d3320907855ed3635541bc85a6710f5
Dustin J. Mitchell <dustin.mitchell@datadoghq.com>
2022-04-18 19:27:48 +0000

    [WIP] Use svc.FilterTemplates for overriding OOTB configs
    If a pod or container annotation specifies a check configuration, then
    any configuration for that check found in files should be ignored.
    
    As a further undocumented feature, a pod or container annotation containing
    `ad.datadoghq.com/<container>.checks: {}` or
    `com.datadoghq.ad.check_names: []` will override any configs associated
    with the container from other sources.
    
    Both of these behaviors were implemented in the config resolver, by
    returning an error when these situations occurred.  This change uses
    svc.FilterTemplates to accomplish the same behavior in a more natural
    fashion.  This change is controlled by a feature flag.
    
    TODO:
     - test
     - find the docs for this feature and verify that all of the annotation
       forms are covered
    
    NOTES:
     - When the feature flag is removed, do we still need
       Service#GetCheckNames?
     - Would it make sense to have a Service implementation specifically for
       containers, rather than using the generic service?

--- pkg/autodiscovery/listeners/service.go
+++ pkg/autodiscovery/listeners/service.go
@@ -11,7 +11,9 @@ import (
 	"reflect"
 
 	"github.com/DataDog/datadog-agent/pkg/autodiscovery/integration"
+	"github.com/DataDog/datadog-agent/pkg/autodiscovery/providers/names"
 	"github.com/DataDog/datadog-agent/pkg/tagger"
+	"github.com/DataDog/datadog-agent/pkg/util"
 	"github.com/DataDog/datadog-agent/pkg/util/containers"
 	"github.com/DataDog/datadog-agent/pkg/util/kubernetes/kubelet"
 	"github.com/DataDog/datadog-agent/pkg/util/log"
@@ -119,6 +121,55 @@ func (s *service) HasFilter(filter containers.FilterType) bool {
 
 // FilterTemplates implements Service#FilterTemplates.
 func (s *service) FilterTemplates(configs map[string]integration.Config) {
+	s.filterTemplatesEmptyOverrides(configs)
+	s.filterTemplatesOverriddenChecks(configs)
+}
+
+// filterTemplatesEmptyOverrides drops file-based templates if this service is a container
+// or pod and has an empty check_names label/annotation.
+func (s *service) filterTemplatesEmptyOverrides(configs map[string]integration.Config) {
+	if !util.CcaUseBareConfigs() {
+		// only applies when `logs_config.cca_use_bare_configs` is set; otherwise this is
+		// handled in pkg/autodiscovery/configresolver/configresolver.go
+		return
+	}
+
+	// Empty check names on k8s annotations or container labels override the check config from file
+	// Used to deactivate unneeded OOTB autodiscovery checks defined in files
+	// The checkNames slice is considered empty also if it contains one single empty string
+	if s.checkNames != nil && (len(s.checkNames) == 0 || (len(s.checkNames) == 1 && s.checNames[0] == "")) {
+		// ...remove all file-based templates
+		for digest, config := range configs {
+			if config.Provider == names.File {
+				log.Debugf(
+					"Ignoring config from %s, as the service %s defines an empty set of checkNames",
+					tpl.Source, s.GetServiceID())
+				delete(configs, digest)
+			}
+		}
+	}
+}
+
+// filterTemplatesOverriddenChecks drops file-based templates if this service's
+// labels/annotations specify a check of the same name.
+func (s *service) filterTemplatesOverriddenChecks(configs map[string]integration.Config) {
+	if !util.CcaUseBareConfigs() {
+		// only applies when `logs_config.cca_use_bare_configs` is set; otherwise this is
+		// handled in pkg/autodiscovery/configresolver/configresolver.go
+		return
+	}
+
+	for digest, config := range configs {
+		for _, checkName := range s.checkNames {
+			if config.Name == checkName {
+				// Ignore config from file when the same check is activated on
+				// the same service via other config providers (k8s annotations
+				// or container labels)
+				log.Debugf("ignoring config from %s: the service %s override check %s",
+					tpl.Source, s.GetServiceID(), tpl.Name)
+			}
+		}
+	}
 }
 
 // GetExtraConfig returns extra configuration associated with the service.
