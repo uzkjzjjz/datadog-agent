2849cc08833e4cd26bc512ab07b9b01d787e919b
Bryce Kahle <bryce.kahle@datadoghq.com>
2022-05-27 12:36:33 -0700

    Remove clusterchecks build tag

--- tasks/build_tags.py
+++ tasks/build_tags.py
@@ -106,7 +106,7 @@ PROCESS_AGENT_HEROKU_TAGS = PROCESS_AGENT_TAGS.difference(
 SECURITY_AGENT_TAGS = {"netcgo", "secrets", "docker", "containerd", "kubeapiserver", "kubelet", "podman", "zlib"}
 
 # SYSTEM_PROBE_TAGS lists the tags necessary to build system-probe
-SYSTEM_PROBE_TAGS = {"netcgo", "clusterchecks", "linux_bpf", "npm"}
+SYSTEM_PROBE_TAGS = {"netcgo", "linux_bpf", "npm"}
 
 # TRACE_AGENT_TAGS lists the tags that have to be added when the trace-agent
 TRACE_AGENT_TAGS = {"docker", "containerd", "kubeapiserver", "kubelet", "otlp", "netcgo", "podman", "secrets"}
