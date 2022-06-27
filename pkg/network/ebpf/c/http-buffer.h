c0eba6c396585a5fbe1fb3583d31ff9ac7882796
Usama Saqib <usama.saqib@datadoghq.com>
2022-06-27 14:56:21 +0200

    read_into_buffer optimiation

--- pkg/network/ebpf/c/http-buffer.h
+++ pkg/network/ebpf/c/http-buffer.h
@@ -2,8 +2,6 @@
 #define __HTTP_BUFFER_H
 
 #include "http-types.h"
-#define LOAD_CONSTANT(param, var) asm("%0 = " param " ll" \
-                                      : "=r"(var))
 
 
 // read_into_buffer copies data from an arbitrary memory address into a (statically sized) HTTP buffer.
@@ -12,36 +10,28 @@
 // In a nutshell, we read HTTP_BUFFER_SIZE bytes no matter what and then get rid of garbage data.
 // Please note that even though the memset could be removed with no semantic change to the code,
 // it is still necessary to make the eBPF verifier happy.
-/*
 static __always_inline void read_into_buffer(char *buffer, char *data, size_t data_size) {
     size_t read_sz = 0;
     __builtin_memset(buffer, 0, HTTP_BUFFER_SIZE);
 
-    int x;
-    LOAD_CONSTANT("read_into_buffer", x);
-
     if (data_size < HTTP_BUFFER_SIZE) {
-    }
-    if (data_size >= HTTP_BUFFER_SIZE) {
-	read_sz = HTTP_BUFFER_SIZE;
+	read_sz = data_size % HTTP_BUFFER_SIZE;
     } else {
-	    read_sz = data_size % HTTP_BUFFER_SIZE;
+	read_sz = HTTP_BUFFER_SIZE;
     }
-    
 
     bpf_probe_read(buffer, read_sz, data);
 
     return;
 }
-*/
 
+/*
 static __always_inline void read_into_buffer(char *buffer, char *data, size_t data_size) {
     __builtin_memset(buffer, 0, HTTP_BUFFER_SIZE);
     bpf_probe_read(buffer, HTTP_BUFFER_SIZE, data);
     if (data_size >= HTTP_BUFFER_SIZE) {
         return;
     }
-/*
 
 #define BLOCK_SIZE (8)
 
@@ -78,7 +68,6 @@ static __always_inline void read_into_buffer(char *buffer, char *data, size_t da
         buffer[7] = 0;
     }
 #undef BLOCK_SIZE
-*/
 }
-
+*/
 #endif
