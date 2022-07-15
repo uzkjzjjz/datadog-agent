aecc5ada2166db58f3ef2a43a503d6fc2378d59d
Usama Saqib <usama.saqib@datadoghq.com>
2022-07-15 22:15:43 +0200

    comments added

--- pkg/network/ebpf/c/http-buffer.h
+++ pkg/network/ebpf/c/http-buffer.h
@@ -3,6 +3,9 @@
 
 #include "http-types.h"
 
+// This function reads a constant number of bytes into the fragment buffer of the http
+// transaction object, and returns the number of bytes of the valid data. The number of
+// bytes are used in userspace to zero out the garbage we may have read into the buffer.
 static __always_inline size_t read_into_buffer(char *buffer, char *data, size_t data_size) {
     __builtin_memset(buffer, 0, HTTP_BUFFER_SIZE);
     int err = bpf_probe_read_user(buffer, HTTP_BUFFER_SIZE, data);
@@ -12,6 +15,9 @@ static __always_inline size_t read_into_buffer(char *buffer, char *data, size_t 
     if (data_size < HTTP_BUFFER_SIZE)
 	    return data_size;
     else
+	    // ensure that the last byte is always
+	    // treated as junk in the userspace and 
+	    // zeroed out. This prevent a buffer overrun.
 	    return HTTP_BUFFER_SIZE-1;
 
 }
