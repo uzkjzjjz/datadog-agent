8475631149f98f0f642675ff2ba36a5aae72c79e
Nicolas PLANEL <nicolas.planel@datadoghq.com>
2022-07-15 20:46:37 +0200

    add monitor unittest

--- pkg/network/http/model.go
+++ pkg/network/http/model.go
@@ -20,9 +20,10 @@ import (
 import "C"
 
 const (
-	HTTPBatchSize  = int(C.HTTP_BATCH_SIZE)
-	HTTPBatchPages = int(C.HTTP_BATCH_PAGES)
-	HTTPBufferSize = int(C.HTTP_BUFFER_SIZE)
+	httpIoctlEnqueue = int(C.HTTP_ENQUEUE)
+	HTTPBatchSize    = int(C.HTTP_BATCH_SIZE)
+	HTTPBatchPages   = int(C.HTTP_BATCH_PAGES)
+	HTTPBufferSize   = int(C.HTTP_BUFFER_SIZE)
 )
 
 type httpTX C.http_transaction_t
