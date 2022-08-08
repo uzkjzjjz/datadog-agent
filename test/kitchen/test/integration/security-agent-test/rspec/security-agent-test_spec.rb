5c53ef791a8e7c79c460049082e5358a548861a9
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-08 12:21:52 +0200

    CWS: use popen2e for output and display test failures

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -1,22 +1,40 @@
 require 'spec_helper'
+require 'open3'
+
+def check_output(output, wait_thr)
+  test_failures = []
+
+  output.each_line do |line|
+    puts line.strip
+    test_failures << line.strip if line =~ GOLANG_TEST_FAILURE
+  end
+
+  if test_failures.empty? && !wait_thr.value.success?
+    test_failures << "Test command exited with status (#{wait_thr.value.exitstatus}) but no failures were captured."
+  end
+
+  test_failures
+end
 
 print `cat /etc/os-release`
 print `uname -a`
 
 describe 'functional test running directly on host' do
   it 'successfully runs' do
-    output = `sudo /tmp/security-agent/testsuite -test.v -status-metrics 1>&2`
-    retval = $?
-    expect(retval).to eq(0)
+    Open3.popen2e("sudo", "/tmp/security-agent/testsuite", "-test.v", "-status-metrics") do |_, output, wait_thr|
+      test_failures = check_output(output, wait_thr)
+      expect(test_failures).to be_empty, test_failures.join("\n")
+    end
   end
 end
 
 if File.readlines("/etc/os-release").grep(/SUSE/).size == 0 and ! File.exists?('/etc/rhsm')
   describe 'functional test running inside a container' do
     it 'successfully runs' do
-      output = `sudo docker exec -ti docker-testsuite /tmp/security-agent/testsuite -test.v -status-metrics --env docker 1>&2`
-      retval = $?
-      expect(retval).to eq(0)
+      Open3.popen2e("sudo", "docker", "exec", "-ti", "docker-testsuite", "/tmp/security-agent/testsuite", "-test.v", "-status-metrics", "--env", "docker") do |_, output, wait_thr|
+        test_failures = check_output(output, wait_thr)
+        expect(test_failures).to be_empty, test_failures.join("\n")
+      end
     end
   end
 end
