2978bd21f7916dcada8052737e60bf525012bb2f
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-08-08 12:12:55 +0200

    CWS: changing describe and it values

--- test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
+++ test/kitchen/test/integration/security-agent-test/rspec/security-agent-test_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 print `cat /etc/os-release`
 print `uname -a`
 
-describe 'successfully run functional test' do
-  it 'displays PASS and returns 0' do
+describe 'functional test running directly on host' do
+  it 'successfully runs' do
     output = `sudo /tmp/security-agent/testsuite -test.v -status-metrics 1>&2`
     retval = $?
     expect(retval).to eq(0)
@@ -12,8 +12,8 @@ describe 'successfully run functional test' do
 end
 
 if File.readlines("/etc/os-release").grep(/SUSE/).size == 0 and ! File.exists?('/etc/rhsm')
-  describe 'successfully run functional test inside a container' do
-    it 'displays PASS and returns 0' do
+  describe 'functional test running inside a container' do
+    it 'successfully runs' do
       output = `sudo docker exec -ti docker-testsuite /tmp/security-agent/testsuite -test.v -status-metrics --env docker 1>&2`
       retval = $?
       expect(retval).to eq(0)
