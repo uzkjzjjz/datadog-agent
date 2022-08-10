6b2ae01a6d8dc66b93d963437ec241848bdb03bc
Scott Opell <me@scottopell.com>
2022-08-10 17:29:37 -0400

    Clarifies file-position instructions, behavior depends on previous value of `start_position` in certain cases

--- tools/agent_QA/test_cases/xplat/helpers.py
+++ tools/agent_QA/test_cases/xplat/helpers.py
@@ -15,10 +15,11 @@ def confDir(config):
 def filePositionSharedSteps():
     return """
 Repeat the steps with:
-- `start_position: end` (with an existing `registry.json` so it picks up where it left off) - should tail from the cursor in registry
-- `start_position: beginning` (with an existing `registry.json` so it picks up where it left off) - should tail from the cursor in registry
+- `start_position: end` (with an existing `registry.json` for this source, previous `start_position` should be `beginning`) should tail from the end of the file, not the offset in registry (ie, same as `forceEnd`)
+- `start_position: end` (with an existing `registry.json` for this source, previous `start_position` should be `end`) should tail from the offset in registry
+- `start_position: beginning` (with an existing `registry.json` for this source, previous `start_position` should be `end`) - should tail from the offset in registry
 - `start_position: end` and delete the `registry.json` first - should start from the end
 - `start_position: beginning` and delete the `registry.json` first - should start from the beginning
-- `start_position: forceBeginning` (with an existing `registry.json` so it picks up where it left off) - should tail from the beginning, ignoring the cursor from the registry
-- `start_position: forceEnd` (with an existing `registry.json` so it picks up where it left off) - should tail from the end, ignoring the cursor from the registry
+- `start_position: forceBeginning` (with an existing `registry.json` for this source) - should tail from the beginning, ignoring the offset from the registry
+- `start_position: forceEnd` (with an existing `registry.json` for this source) - should tail from the end, ignoring the offset from the registry
 """
